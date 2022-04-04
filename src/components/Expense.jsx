82008b9e499d6f445965ea0fc18f4ddd510a9d7d
bludbruda1 <g_topeto21@hotmail.com>
2022-04-05 10:54:36 +1200

    add in responsiveness

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,12 +1,23 @@
 import AddIcon from "@mui/icons-material/Add";
 import Box from "@mui/material/Box";
 import DeleteIcon from "@mui/icons-material/Delete";
-import FormControl from "@mui/material/FormControl";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import React, { useState } from "react";
+import { createMedia } from "@artsy/fresnel";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+
+const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    sm: 0,
+    md: 768,
+    lg: 1024,
+    xl: 1192,
+  },
+});
 
 export default function Expense() {
   const [valuesList, setValueList] = useState([
@@ -38,53 +49,111 @@ export default function Expense() {
   };
 
   return (
-    <FormControl sx={{ width: "75ch" }}>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: 575 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        {valuesList.map((singleExpense, index) => (
-          <div key={index} className="expenses">
-            <Stack spacing={2} direction="row">
-              <TextField variant="outlined" label="Expense" />
-              <TextField
-                name="expense"
-                type="number"
-                variant="outlined"
-                label="Amount"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">$</InputAdornment>
-                  ),
-                }}
-                value={singleExpense.expense}
-                onChange={(e) => handleValuesChange(e, index)}
-              />
-              {valuesList.length > 1 && (
-                <IconButton
-                  aria-label="delete"
-                  onClick={() => handleValuesRemove(index)}
-                >
-                  <DeleteIcon />{" "}
-                </IconButton>
-              )}
-              {valuesList.length - 1 === index && (
-                <IconButton
-                  aria-label="add"
-                  color="primary"
-                  onClick={handleValuesAdd}
-                >
-                  <AddIcon />
-                </IconButton>
-              )}
-            </Stack>
-          </div>
-        ))}
-      </Box>
-    </FormControl>
+    <MediaContextProvider>
+      <Container>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: 575 },
+          }}
+        >
+          {valuesList.map((singleExpense, index) => (
+            <div key={index} className="expenses">
+              <Stack spacing={2} direction="row">
+                <Media lessThan="lg">
+                  <Grid container direction="column" columns={1} spacing={2}>
+                    <Grid item>
+                      <Stack spacing={2} direction="column">
+                        <TextField
+                          variant="outlined"
+                          label="Expense"
+                          sx={{ width: 200 }}
+                        />
+                      </Stack>
+                    </Grid>
+                    <Grid item>
+                      <Stack spacing={1} direction="row">
+                        <TextField
+                          sx={{ width: 200 }}
+                          name="expense"
+                          type="number"
+                          variant="outlined"
+                          label="Amount"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                $
+                              </InputAdornment>
+                            ),
+                          }}
+                          value={singleExpense.expense}
+                          onChange={(e) => handleValuesChange(e, index)}
+                        />
+                        {valuesList.length > 1 && (
+                          <IconButton
+                            aria-label="delete"
+                            onClick={() => handleValuesRemove(index)}
+                          >
+                            <DeleteIcon />{" "}
+                          </IconButton>
+                        )}
+                      </Stack>
+                    </Grid>
+                    <Grid item>
+                      <Stack spacing={2} direction="row">
+                        {valuesList.length - 1 === index && (
+                          <IconButton
+                            aria-label="add"
+                            color="primary"
+                            onClick={handleValuesAdd}
+                          >
+                            <AddIcon />
+                          </IconButton>
+                        )}
+                      </Stack>
+                    </Grid>
+                  </Grid>
+                </Media>
+                <Media greaterThanOrEqual="lg">
+                  <Stack spacing={2} direction="row">
+                    <TextField variant="outlined" label="Expense" />
+                    <TextField
+                      name="expense"
+                      type="number"
+                      variant="outlined"
+                      label="Amount"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">$</InputAdornment>
+                        ),
+                      }}
+                      value={singleExpense.expense}
+                      onChange={(e) => handleValuesChange(e, index)}
+                    />
+                    {valuesList.length > 1 && (
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => handleValuesRemove(index)}
+                      >
+                        <DeleteIcon />{" "}
+                      </IconButton>
+                    )}
+                  </Stack>
+                  {valuesList.length - 1 === index && (
+                    <IconButton
+                      aria-label="add"
+                      color="primary"
+                      onClick={handleValuesAdd}
+                    >
+                      <AddIcon />
+                    </IconButton>
+                  )}
+                </Media>
+              </Stack>
+            </div>
+          ))}
+        </Box>
+      </Container>
+    </MediaContextProvider>
   );
 }
